897957e18fbd34872b913895201176577c3a077d
Norwick Kin On Lee <norwick.ko.lee@gmail.com>
2021-07-26 08:28:38 -0700

    add base project
    starter project added
    update .gitignore
    check git ignore working before adding passwords in
    git ignore is working
    add mockito dependency

---
+++ reciperodeo/src/main/java/com/github/norwick/reciperodeo/RecipeRodeoApplication.java
@@ -0,0 +1,13 @@
+package com.github.norwick.reciperodeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeRodeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeRodeoApplication.class, args);
+	}
+
+}
