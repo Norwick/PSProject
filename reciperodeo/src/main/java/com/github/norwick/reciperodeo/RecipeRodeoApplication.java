8a2db93cf886139211420c0e1ee49eb9d1dae5fb
Norwick Kin On Lee <norwick.ko.lee@gmail.com>
2021-07-26 08:55:39 -0700

    hello world

--- reciperodeo/src/main/java/com/github/norwick/reciperodeo/RecipeRodeoApplication.java
+++ reciperodeo/src/main/java/com/github/norwick/reciperodeo/RecipeRodeoApplication.java
@@ -3,9 +3,13 @@ package com.github.norwick.reciperodeo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Tells Spring to load all configurations and create the application
+ */
 @SpringBootApplication
 public class RecipeRodeoApplication {
 
+	@SuppressWarnings("javadoc")
 	public static void main(String[] args) {
 		SpringApplication.run(RecipeRodeoApplication.class, args);
 	}
